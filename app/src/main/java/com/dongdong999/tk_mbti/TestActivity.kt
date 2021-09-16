99e32e8f8c29543fa5631444865669d00785514c
N0mercy00 <dkdhehesp@naver.com>
2021-09-16 09:12:07 +0900

    Calculate score is work

--- app/src/main/java/com/dongdong999/tk_mbti/TestActivity.kt
+++ app/src/main/java/com/dongdong999/tk_mbti/TestActivity.kt
@@ -12,8 +12,10 @@ class TestActivity : AppCompatActivity() {
         setContentView(binding.root)
         var tempPoint : Int =0;
         var IEPoint : Int =0;
+        var IECalPoint : Double=0.0
         var IE1 : Int =0;
         var IE2 : Int =0;
+        var IE3 : Int =0;
         var SNpoint : Int =0;
 
 
@@ -74,10 +76,44 @@ class TestActivity : AppCompatActivity() {
             IE2=tempPoint
         }
 
+        binding.rgIE3.setOnCheckedChangeListener { group,id->
+            IE3=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_IE3NN->{
+                    tempPoint=0
+                    tempPoint-=2
+
+                }
+                R.id.rb_IE3N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_IE3M->{
+                    tempPoint=0
+                }
+                R.id.rb_IE3Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_IE3YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            IE3=tempPoint
+        }
+
         binding.btnGotoResult.setOnClickListener {
-            IEPoint=IE1+IE2
+
+            IEPoint=0
+            IEPoint=IE1+IE2+IE3
             Log.d("TAG","IE1 점수 : ${IE1},IE2 점수 : ${IE2}")
             Log.d("TAG","현재 토탈 IE 점수 : ${IEPoint}")
+
+            IECalPoint=(IEPoint.toDouble()/3)*50
+            Log.d("TAG","환산 IE 점수 : ${IECalPoint}")
+
         }
 
 
