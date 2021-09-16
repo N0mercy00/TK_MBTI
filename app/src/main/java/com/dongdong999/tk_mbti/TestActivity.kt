0b337c1630d975baea2d083de638ef69a2e45cdb
N0mercy00 <dkdhehesp@naver.com>
2021-09-16 10:29:06 +0900

    Damm why i does not use ListView or RecyclerView Damm!

--- app/src/main/java/com/dongdong999/tk_mbti/TestActivity.kt
+++ app/src/main/java/com/dongdong999/tk_mbti/TestActivity.kt
@@ -10,8 +10,9 @@ class TestActivity : AppCompatActivity() {
 
         var tempPoint : Int =0; //임시값
         var IEqusetion :Array<Int> = arrayOf(0,0,0)//문제별 점수값값
-
         var SNqusetion :Array<Int> = arrayOf(0,0,0)
+        var TFqusetion :Array<Int> = arrayOf(0,0,0)
+        var JPqusetion :Array<Int> = arrayOf(0,0,0)
 
         var CalPoint:Array<Double> = arrayOf(0.0,0.0,0.0,0.0)// 환산점수 각 배열의 요소가 IE SN TF JP 점수
                                                             // -100->E  100->I
@@ -194,15 +195,182 @@ class TestActivity : AppCompatActivity() {
         //
 
         //TF_Part
+        binding.rgTF1.setOnCheckedChangeListener { group,id->
 
+            TFqusetion[0]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_TF1NN->{
+                    tempPoint=0
+                    tempPoint-=2
+                }
+                R.id.rb_TF1N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_TF1M->{
+                    tempPoint=0
+                }
+                R.id.rb_TF1Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_TF1YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            TFqusetion[0]=tempPoint
+        }
+        binding.rgTF2.setOnCheckedChangeListener { group,id->
+            TFqusetion[1]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_TF2NN->{
+                    tempPoint=0
+                    tempPoint-=2
+
+                }
+                R.id.rb_TF2N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_TF2M->{
+                    tempPoint=0
+                }
+                R.id.rb_TF2Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_TF2YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            TFqusetion[1]=tempPoint
+        }
+        binding.rgTF3.setOnCheckedChangeListener { group,id->
+            TFqusetion[2]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_TF3NN->{
+                    tempPoint=0
+                    tempPoint-=2
+
+                }
+                R.id.rb_TF3N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_TF3M->{
+                    tempPoint=0
+                }
+                R.id.rb_TF3Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_TF3YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            TFqusetion[2]=tempPoint
+        }
+        //
+
+        //JP_part
+        binding.rgJP1.setOnCheckedChangeListener { group,id->
+            JPqusetion[0]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_JP1NN->{
+                    tempPoint=0
+                    tempPoint-=2
+                }
+                R.id.rb_JP1N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_JP1M->{
+                    tempPoint=0
+                }
+                R.id.rb_JP1Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_JP1YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            JPqusetion[0]=tempPoint
+        }
+        binding.rgJP2.setOnCheckedChangeListener { group,id->
+            JPqusetion[1]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_JP2NN->{
+                    tempPoint=0
+                    tempPoint-=2
+                }
+                R.id.rb_JP2N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_JP2M->{
+                    tempPoint=0
+                }
+                R.id.rb_JP2Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_JP2YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            JPqusetion[1]=tempPoint
+        }
+        binding.rgJP3.setOnCheckedChangeListener { group,id->
+            JPqusetion[2]=tempPoint
+            tempPoint=0
+            when(id){
+                R.id.rb_JP3NN->{
+                    tempPoint=0
+                    tempPoint-=2
+                }
+                R.id.rb_JP3N->{
+                    tempPoint=0
+                    tempPoint-=1
+                }
+                R.id.rb_JP3M->{
+                    tempPoint=0
+                }
+                R.id.rb_JP3Y->{
+                    tempPoint=0
+                    tempPoint+=1
+                }
+                R.id.rb_JP3YY->{
+                    tempPoint=0
+                    tempPoint+=2
+                }
+            }
+            JPqusetion[2]=tempPoint
+        }
         //
 
         binding.btnGotoResult.setOnClickListener {
             CalPoint[0]=((IEqusetion[0]+IEqusetion[1]+IEqusetion[2]).toDouble()/3)*50
-            Log.d("TAG","환산 IE 점수 : ${CalPoint[0]}")
+            Log.d("TAG","\n환산 IE 점수 : ${CalPoint[0]}")
 
             CalPoint[1]=((SNqusetion[0]+SNqusetion[1]+SNqusetion[2]).toDouble()/3)*50
             Log.d("TAG","환산 SN 점수 : ${CalPoint[1]}")
+
+            CalPoint[2]=((TFqusetion[0]+TFqusetion[1]+TFqusetion[2]).toDouble()/3)*50
+            Log.d("TAG","환산 TF 점수 : ${CalPoint[2]}")
+
+            CalPoint[3]=((JPqusetion[0]+JPqusetion[1]+JPqusetion[2]).toDouble()/3)*50
+            Log.d("TAG","환산 JP 점수 : ${CalPoint[3]}")
 
         }
 
