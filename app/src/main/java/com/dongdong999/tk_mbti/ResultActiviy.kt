a23559e47e122e89c441cdbe38f6ca75172e7623
N0mercy00 <dkdhehesp@naver.com>
2021-09-17 13:57:40 +0900

    Update of Heroes

--- app/src/main/java/com/dongdong999/tk_mbti/ResultActiviy.kt
+++ app/src/main/java/com/dongdong999/tk_mbti/ResultActiviy.kt
@@ -36,21 +36,126 @@ class ResultActiviy : AppCompatActivity() {
         Log.d("TAG","전달완료 결과값 ${resultText}, 점수 각 ${score[0]} ${score[1]} ${score[2]} ${score[3]}")
 
 
-        if(resultText.equals("ISTJ")){
+
+        if(resultText.equals("ENFP")){
             Glide.with(this)
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/GuanYu-ZhouCang.jpg/200px-GuanYu-ZhouCang.jpg")
+                .load(getString(R.string.ENFP_url))
                 .into(binding.ivResultImage)
 
-
-
-        }else if(resultText.equals("ENFP")){
-            Glide.with(this)
-                .load("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Cao_Cao_Portrait_ROTK.jpg/209px-Cao_Cao_Portrait_ROTK.jpg")
-                .into(binding.ivResultImage)
             binding.tvResultName.text=getString(R.string.ENFP_title)
             binding.tvResultsay.text = getString(R.string.ENFP_say)
             binding.tvResultsummary.text=getText(R.string.ENFP_content)
+
+
+        }else if(resultText.equals("ENTJ")){
+            Glide.with(this)
+                .load(getString(R.string.ENTJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ENTJ_title)
+            binding.tvResultsay.text = getString(R.string.ENTJ_say)
+            binding.tvResultsummary.text=getText(R.string.ENTJ_content)
+        }else if(resultText.equals("ESTJ")){
+            Glide.with(this)
+                .load(getString(R.string.ESTJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ESTJ_title)
+            binding.tvResultsay.text = getString(R.string.ESTJ_say)
+            binding.tvResultsummary.text=getText(R.string.ESTJ_content)
+
+        }else if(resultText.equals("INFP")){
+            Glide.with(this)
+                .load(getString(R.string.INFP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.INFP_title)
+            binding.tvResultsay.text = getString(R.string.INFP_say)
+            binding.tvResultsummary.text=getText(R.string.INFP_content)
+        }else if(resultText.equals("ESTP")){
+            Glide.with(this)
+                .load(getString(R.string.ESTP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ESTP_title)
+            binding.tvResultsay.text = getString(R.string.ESTP_say)
+            binding.tvResultsummary.text=getText(R.string.ESTP_content)
+        }else if(resultText.equals("ENTP")){
+            Glide.with(this)
+                .load(getString(R.string.ENTP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ENTP_title)
+            binding.tvResultsay.text = getString(R.string.ENTP_say)
+            binding.tvResultsummary.text=getText(R.string.ENTP_content)
+        }else if(resultText.equals("ISTJ")){
+            Glide.with(this)
+                .load(getString(R.string.ISTJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ISTJ_title)
+            binding.tvResultsay.text = getString(R.string.ISTJ_say)
+            binding.tvResultsummary.text=getText(R.string.ISTJ_content)
+        }else if(resultText.equals("ISTP")){
+            Glide.with(this)
+                .load(getString(R.string.ISTP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ISTP_title)
+            binding.tvResultsay.text = getString(R.string.ISTP_say)
+            binding.tvResultsummary.text=getText(R.string.ISTP_content)
+        }else if(resultText.equals("INFJ")){
+            Glide.with(this)
+                .load(getString(R.string.INFJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.INFJ_title)
+            binding.tvResultsay.text = getString(R.string.INFJ_say)
+            binding.tvResultsummary.text=getText(R.string.INFJ_content)
+        }else if(resultText.equals("INTP")){
+            Glide.with(this)
+                .load(getString(R.string.INTP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.INTP_title)
+            binding.tvResultsay.text = getString(R.string.INTP_say)
+            binding.tvResultsummary.text=getText(R.string.INTP_content)
+        }else if(resultText.equals("ENFJ")){
+            Glide.with(this)
+                .load(getString(R.string.ENFJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ENFJ_title)
+            binding.tvResultsay.text = getString(R.string.ENFJ_say)
+            binding.tvResultsummary.text=getText(R.string.ENFJ_content)
+        }else if(resultText.equals("ESFJ")){
+            Glide.with(this)
+                .load(getString(R.string.ESFJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ESFJ_title)
+            binding.tvResultsay.text = getString(R.string.ESFJ_say)
+            binding.tvResultsummary.text=getText(R.string.ESFJ_content)
+        }else if(resultText.equals("ISFJ")){
+            Glide.with(this)
+                .load(getString(R.string.ISFJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ISFJ_title)
+            binding.tvResultsay.text = getString(R.string.ISFJ_say)
+            binding.tvResultsummary.text=getText(R.string.ISFJ_content)
+        }else if(resultText.equals("INTJ")){
+            Glide.with(this)
+                .load(getString(R.string.INTJ_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.INTJ_title)
+            binding.tvResultsay.text = getString(R.string.INTJ_say)
+            binding.tvResultsummary.text=getText(R.string.INTJ_content)
+        }else if(resultText.equals("ISFP")){
+            Glide.with(this)
+                .load(getString(R.string.ISFP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ISFP_title)
+            binding.tvResultsay.text = getString(R.string.ISFP_say)
+            binding.tvResultsummary.text=getText(R.string.ISFP_content)
+        }else if(resultText.equals("ESFP")){
+            Glide.with(this)
+                .load(getString(R.string.ESFP_url))
+                .into(binding.ivResultImage)
+            binding.tvResultName.text=getString(R.string.ESFP_title)
+            binding.tvResultsay.text = getString(R.string.ESFP_say)
+            binding.tvResultsummary.text=getText(R.string.ESFP_content)
         }
+
+
 
 
 
